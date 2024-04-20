398834367115e37737664d76f4b595f78646fd89
SdZika <markozivkovic1982@gmail.com>
2024-04-20 14:31:46 +0200

    done basic setup for html css and js

--- main.js
+++ main.js
@@ -50,3 +50,60 @@ function generateSequence(limit) {
     }
     return sequence;
 }*/
+function processInput (){
+    let input = document.getElementById("value-input").value;
+    let selectFunction = document.getElementById("select-function").value;
+    let output;
+  
+    switch (selectFunction){
+      case "factorial":
+        output = factorialNumber(input);
+        break;
+      case "reverse-word":
+        output = reverseWord(input);
+        break;
+      case "uppercase":
+        output = makeUpperCase(input);
+        break;
+      case "loop":
+        output = squareNumber(input);
+        break;
+      default:
+        output = "Invalid input";
+    }
+  
+    document.getElementById('output-field').innerText = output;
+  }
+  
+  //factorial function
+  function factorialNumber(num) {
+    if (num < 0) return "Cannot calculate factorial of a negative number";
+    if (num === 0 || num === 1) return 1;
+    let sum = 1;
+    for (let i = 1; i <= num; i++) {
+      sum = sum * i;
+    }
+    return sum;
+  }
+  
+  //reverse word
+  function reverseWord(word) {
+    return word.split("").reverse().join("");
+  }
+  
+  //to upper case
+  function makeUpperCase(word){
+    return word.toUpperCase();
+  }
+  
+  
+  //loop example
+  function squareNumber(num) {
+    let sequence = '';
+    for (let i = 1; i <= num; i++) {
+        sequence += `${i}: ${i * i}\n`;
+    }
+    return sequence;
+  }
+  
+  
